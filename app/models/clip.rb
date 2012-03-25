eda8819979bf1fdc02e2a4cd93eb8559968f21b6
Hiroki Kondo <kompiro@gmail.com>
2012-03-25 23:16:35 +0900

    added to load data

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,2 +1,30 @@
+require 'open-uri'
+require 'nokogiri'
+
 class Clip < ActiveRecord::Base
+  def load
+    return false if url.nil? or url.empty?
+    return false unless url.start_with?('http:') or url.start_with?('https:')
+    doc = Nokogiri::HTML(open(url))
+    doc.css('meta').each do |m|
+      prop = m.attribute('property')
+      if prop
+        if prop.to_s.match(/^og:title/i)
+          self.title = m.attribute('content').to_s
+        end
+        if prop.to_s.match(/^og:type/i)
+          self.type = m.attribute('content').to_s
+        end
+        if prop.to_s.match(/^og:image/i)
+          self.image = m.attribute('content').to_s
+        end
+        if prop.to_s.match(/^og:url/i)
+          self.url = m.attribute('content').to_s
+        end
+        if prop.to_s.match(/^og:description/i)
+          self.description = m.attribute('content').to_s
+        end
+      end
+    end
+  end
 end
--- spec/models/clip_spec.rb
+++ spec/models/clip_spec.rb
@@ -1,5 +1,52 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Clip do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'attributes' do
+    subject{Clip.new}
+    its(:title)       {should be_nil}
+    its(:type)        {should be_nil}
+    its(:image)       {should be_nil}
+    its(:description) {should be_nil}
+    its(:url)         {should be_nil}
+  end
+  context 'load og content' do
+    before do
+      doc = Nokogiri::HTML(open("#{Rails.root}/spec/support/ogp/#{file}"))
+      Nokogiri::HTML::Document.stub!(:parse).and_return(doc)
+      @clip = Clip.new(:url => url)
+      @clip.load
+    end
+    subject{@clip}
+    let(:file){'empty.html'}
+    context 'error cases' do
+      describe 'url is nil case' do
+        let(:url){nil}
+        its(:title)   {should be_nil}
+      end
+      describe 'url is empty string case' do
+        let(:url){''}
+        its(:title)   {should be_nil}
+      end
+      context 'illegal url' do
+        let(:url){'//example.com/empty.html'}
+        its(:title)  {should be_nil}
+      end
+      describe 'load empty content' do
+        let(:url){'http://example.com/empty.html'}
+        its(:title)  {should be_nil}
+      end
+    end
+    context 'normal cases' do
+      describe 'load content' do
+        let(:file){'mine.html'}
+        let(:url){'http://example.com/mine.html'}
+        its(:title)  {should == 'Fly me to the Juno!'}
+        its(:type)  {should == 'blog'}
+        its(:image)  {should == 'http://www.st-hatena.com/users/ko/kompiro/user_p.gif?'}
+        its(:url)  {should == 'http://d.hatena.ne.jp/kompiro/'}
+        its(:description)  {should == 'Planet Eclipseにも参加しています。ソリューションログを軸に書いてます。'}
+      end
+    end
+  end
 end
