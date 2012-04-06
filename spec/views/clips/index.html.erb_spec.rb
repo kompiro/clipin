465de86dcef25ddfd1c820fb0e9b05a4d2f2ad9a
Hiroki Kondo <kompiro@gmail.com>
2012-04-06 23:57:32 +0900

    removed spec because there are no initial html code

--- spec/views/clips/index.html.erb_spec.rb
+++ spec/views/clips/index.html.erb_spec.rb
@@ -22,9 +22,5 @@ describe "clips/index" do
 
   it "renders a list of clips" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select ".title", :text => "MyText".to_s, :count => 2
-    assert_select ".description", :text => "MyText".to_s, :count => 2
-    assert_select ".og_type", :text => "Type".to_s, :count => 2
   end
 end
