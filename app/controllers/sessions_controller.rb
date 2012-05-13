6350588547503bd4b9ab4908ee89b8926e0e4c8f
Hiroki Kondo <kompiro@gmail.com>
2012-05-14 00:57:37 +0900

    disable json format access session#new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,7 @@ class SessionsController < ApplicationController
   skip_before_filter :authenticate!, :only => %w[new create]
 
   def new
-    respond_to do |format|
-      format.html { render html: {:error => 'authorized error'}, status: :unauthorized } # new.html.erb
-      format.json { render json: {:error => 'authorized error'}, status: :unauthorized  }
-    end
+    render html: {:error => 'authorized error'}, status: :unauthorized
   end
 
   def delete
