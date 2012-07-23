e9d60817f510cded1634d0f3d6fc4f1770ece6ac
Hiroki Kondo <kompiro@gmail.com>
2012-07-23 22:19:23 +0900

    fix real git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "ssh://#{user}@157.7.128.227/opt/repos/clipin.git"
+set :repository, "file:///opt/repos/clipin"
 set :branch, "master"
 
 default_run_options[:pty] = true
