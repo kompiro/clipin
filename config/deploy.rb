2dd2a461ff198f2f8600d5360227b6ac415d495c
Hiroki Kondo <kompiro@gmail.com>
2012-07-23 22:38:33 +0900

    fix command path

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-set :bundle_cmd, 'sh $HOME/.bashrc && bundle'
+set :bundle_cmd, "/home/#{user}/.rbenv/shims/bundle"
 
 set :scm, "git"
 set :repository, "ssh://deployer@157.7.128.227/opt/repos/clipin"
