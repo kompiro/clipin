00646f52c4abbb18e94cdea917c586587d1255bd
Hiroki Kondo <kompiro@gmail.com>
2012-07-23 22:13:06 +0900

    fix url scheme

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "file:/opt/repos/clipin.git"
+set :repository, "file:///opt/repos/clipin.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
