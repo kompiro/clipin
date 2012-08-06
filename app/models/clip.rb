22b7a70dba4b42734298573428b0364c26fa5f74
Hiroki Kondo <kompiro@gmail.com>
2012-08-06 19:50:58 +0900

    refactoring

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -36,30 +36,13 @@ class Clip < ActiveRecord::Base
     rescue ArgumentError
     end
     self.url = url
-    doc.css('meta').each do |m|
-      prop = m.attribute('property')
-      if prop
-        content = m.attribute('content')
-        if content.nil?
-          next
-        end
-        value = content.text
-        if prop.to_s.match(/^og:type/i)
-          self.og_type = value
-        end
-        if prop.to_s.match(/^og:image/i)
-          self.image = value
-        end
-        if prop.to_s.match(/^og:description/i)
-          self.description = value
-        end
-      end
-    end
+    parse_prop doc
     if self.description.nil?
       self.description = doc.xpath('//meta[@name="description"]/@content').text
     end
     return true
   end
+
   def tagging
     if self.og_type.nil? or self.og_type.empty?
       return
@@ -80,5 +63,27 @@ class Clip < ActiveRecord::Base
       read = read.encode("UTF-8", "UTF-8", :invalid => :replace, :undef => :replace)
     end
     doc = Nokogiri.HTML(read)
+  end
+
+  def parse_prop(doc)
+    doc.css('meta').each do |m|
+      prop = m.attribute('property')
+      if prop
+        content = m.attribute('content')
+        if content.nil?
+          next
+        end
+        value = content.text
+        if prop.to_s.match(/^og:type/i)
+          self.og_type = value
+        end
+        if prop.to_s.match(/^og:image/i)
+          self.image = value
+        end
+        if prop.to_s.match(/^og:description/i)
+          self.description = value
+        end
+      end
+    end
   end
 end
