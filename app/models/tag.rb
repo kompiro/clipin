e5c77bcadab6b7b84d15cb0d5f362c97076bfda9
Hiroki Kondo <kompiro@gmail.com>
2012-09-03 07:10:22 +0900

    fixed to not show when not set User.current

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,16 @@
 class Tag < ActiveRecord::Base
   attr_accessor :page_num
   has_many :taggings
-  has_many :clips, :through => :taggings, :conditions => {:user_id => User.current.id,:trash => false},:order => "clips.created_at DESC",:limit => 8
+  has_many :clips, :through => :taggings,:order => "clips.created_at DESC",:limit => 8
   PAGE_CONTENT = 8
 
   belongs_to :user
 
   def my_clips(page_num = 1)
-    return clips.offset(PAGE_CONTENT * ([page_num.to_i, 1].max - 1))
+    if User.current.nil?
+      return
+    end
+    return clips.where(:user_id => User.current.id,:trash => false).offset(PAGE_CONTENT * ([page_num.to_i, 1].max - 1))
   end
 
   def save
