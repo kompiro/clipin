694ef3ea89262c305f0cb4140e6b0cd9d6abbac0
Hiroki Kondo <kompiro@gmail.com>
2012-09-18 22:07:33 +0900

    improved behavior when unauthorized state

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_filter :authenticate!, :only => %w[new create]
+  layout "unauthorized",:only => [:new,:create]
 
   def new
     render html: {:error => 'authorized error'}, status: :unauthorized
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
   def delete
     session[:user_id] = nil
     cookies.signed[:user_id] = nil
-    redirect_to login_path
+    redirect_to root_path
   end
 
   def create
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
     User.current = user
     session[:user_id] = user.id
     cookies.signed[:user_id] = {:value => user.id,:expires => 1.year.from_now}
-    redirect_to '/clips#index'
+    redirect_to clips_path
   end
 
   protected
