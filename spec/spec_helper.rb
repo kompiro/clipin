834cb6eba77c933f75d436db37694c8b436255cf
Hiroki Kondo <kompiro@gmail.com>
2012-09-25 23:33:16 +0900

    remove fixture setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ Spork.prefork do
     config.mock_with :rspec
 
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
     config.before(:suite) do
       DatabaseCleaner.strategy = :truncation
@@ -88,7 +88,7 @@ RSpec.configure do |config|
   # config.mock_with :rr
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
