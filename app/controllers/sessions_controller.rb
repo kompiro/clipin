ea58b810ec397943827993cf47322a55333317b1
Hiroki Kondo <kompiro@gmail.com>
2012-10-10 23:39:00 +0900

    show login message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,11 +14,20 @@ class SessionsController < ApplicationController
 
   def create
     auth = auth_hash
-    user = User.where(:provider => auth['provider'],
-        :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    user = User.where(:provider => auth['provider'], :uid => auth['uid']).first
+    if user.present?
+      existed = true
+    else
+      user = User.create_with_omniauth(auth)
+    end
     User.current = user
     session[:user_id] = user.id
     cookies.signed[:user_id] = {:value => user.id,:expires => 1.year.from_now}
+    if existed
+      flash[:success] = 'Welcome Back!'
+    else
+      flash[:success] = 'Welcome!'
+    end
     redirect_to clips_path
   end
 
