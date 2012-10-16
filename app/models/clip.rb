c3c52d54cc0882936770810973834b73148d27d1
Hiroki Kondo <kompiro@gmail.com>
2012-10-16 21:57:24 +0900

    fix spec/models

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -11,23 +11,24 @@ class Clip < ActiveRecord::Base
   belongs_to :user
   belongs_to :url_info
   before_save :set_user_before_save
-  delegate :title,:url,:description,:image,:og_type, :to => :url_info
+  delegate :title, :title=, :url, :url=, :description, :description=, :image, :image=, :og_type, :og_type=, :to => :url_info
   PAGE_CONTENT = 8
 
   scope :page,    lambda {|page_num = 1| includes(:tags).where(:user_id => User.current).where(:trash => false).order('updated_at DESC').limit(PAGE_CONTENT).offset(PAGE_CONTENT * ([page_num.to_i, 1].max - 1))}
-  scope :search,    lambda {|query='',page_num = 1| where(:user_id => User.current).where(:trash => false).where('title like ?',"%#{query}%").order('updated_at DESC').limit(PAGE_CONTENT).offset(PAGE_CONTENT * ([page_num.to_i, 1].max - 1))}
+  scope :search,  lambda {|query='',page_num = 1| joins(:url_info).where(:user_id => User.current).where(:trash => false).where('url_infos.title like ?',"%#{query}%").order('updated_at DESC').limit(PAGE_CONTENT).offset(PAGE_CONTENT * ([page_num.to_i, 1].max - 1))}
   scope :pinned,  lambda { where(:user_id => User.current).where(:pin => true,:trash => false).order('updated_at DESC').limit(PAGE_CONTENT)}
   scope :trashed, lambda { where(:user_id => User.current).where(:trash => true).order('updated_at DESC').limit(PAGE_CONTENT)}
 
   def initialize(*args)
     super()
-    @url_info = UrlInfo.new({
-        :url => @url,
-        :title => @title,
-        :image => @image,
-        :og_type => @og_type,
-        :description => @description
-    })
+    self.url_info = UrlInfo.new()
+    return if args.length == 0
+
+    self.url_info.url = args[0][:url]
+    self.url_info.title = args[0][:title]
+    self.url_info.image = args[0][:image]
+    self.url_info.og_type = args[0][:og_type]
+    self.url_info.description = args[0][:description]
   end
 
   def set_user_before_save
--- spec/factories/url_infos.rb
+++ spec/factories/url_infos.rb
@@ -1,11 +1,38 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'forgery'
 
 FactoryGirl.define do
   factory :url_info do
-    title "MyString"
-    url "MyString"
-    type ""
-    description "MyString"
-    image "MyString"
+    title {Forgery(:name).title}
+    image {"http://#{Forgery(:internet).domain_name}/"}
+    url {"http://#{Forgery(:internet).domain_name}/"}
+    og_type ""
+    description "description"
+  end
+  factory :slideshare_info, class: UrlInfo do
+    title {Forgery(:name).title}
+    image {"http://#{Forgery(:internet).domain_name}/"}
+    url {"http://#{Forgery(:internet).domain_name}/"}
+    og_type "slideshare:presentation"
+    description "description"
+  end
+  factory :speakerdeck_info, class: UrlInfo do
+    title {Forgery(:name).title}
+    image {"https://speakerd.s3.amazonaws.com/presentations/4ff713abb5c1770021049c4b/thumb_slide_0.jpg"}
+    url {"https://speakerdeck.com/u/#{Forgery(:name).title}/p/#{Forgery(:name).title}"}
+    og_type ""
+    description "description"
+  end
+  factory :search_info, class: UrlInfo do
+    title {FactoryGirl.generate :test_title}
+    image {"http://#{Forgery(:internet).domain_name}/"}
+    url {"http://#{Forgery(:internet).domain_name}/"}
+    og_type "website"
+    description "description"
+  end
+  FactoryGirl.define do
+    sequence :test_title do |n|
+      "test #{n}"
+    end
   end
 end
