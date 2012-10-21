b6bed1ae4be85a461ffbd5d0ef9cec55244d1b49
Hiroki Kondo <kompiro@gmail.com>
2012-10-21 23:51:18 +0900

    fix illegal using of class attribute User.current

--- spec/factories/clip.rb
+++ spec/factories/clip.rb
@@ -2,42 +2,18 @@ require 'forgery'
 
 FactoryGirl.define do
   factory :clip do
-    title {Forgery(:name).title}
-    image {"http://#{Forgery(:internet).domain_name}/"}
-    url {"http://#{Forgery(:internet).domain_name}/"}
     user
     url_info
     created_at Time.now
     updated_at Time.now
-    before(:create) do |clip|
-      User.current = clip.user
+    factory :slideshare do
+      association :url_info,factory: :slideshare_info
     end
-  end
-  factory :slideshare, class: Clip do
-    user
-    association :url_info,factory: :slideshare_info
-    created_at Time.now
-    updated_at Time.now
-    before(:create) do |clip|
-      User.current = clip.user
+    factory :speakerdeck do
+      association :url_info,factory: :speakerdeck_info
     end
-  end
-  factory :speakerdeck, class: Clip do
-    user
-    association :url_info,factory: :speakerdeck_info
-    created_at Time.now
-    updated_at Time.now
-    before(:create) do |clip|
-      User.current = clip.user
-    end
-  end
-  factory :search_clip , class: Clip  do
-    user
-    association :url_info, factory: :search_info
-    created_at Time.now
-    updated_at Time.now
-    before(:create) do |clip|
-      User.current = clip.user
+    factory :search_clip do
+      association :url_info, factory: :search_info
     end
   end
 end
--- spec/models/clip_spec.rb
+++ spec/models/clip_spec.rb
@@ -11,92 +11,80 @@ describe Clip do
   end
   context 'user' do
     before do
-      User.current = create(:user)
+      @user = create(:user)
+    end
+    it 'should have user after saved',:filter => true do
       @clip = Clip.new(:url => 'http://example.com/')
-      @clip.save
-    end
-    after do
-      User.current = nil
-    end
-    it 'should be created' do
-      User.current.should_not be_nil
-    end
-    it 'should have user after saved' do
-      @clip.user.should_not be_nil
-    end
-    it 'should not overwrite user when User.current is nil' do
-      User.current = nil
+      @clip.user = @user
       @clip.save
       @clip.user.should_not be_nil
     end
   end
   context 'tagging' do
     before do
-      User.current = create(:user)
+      @user = create(:user)
       @clip = clip
+      @clip.user = @user
       @clip.save
       @clip.tagging
-      @clip
-    end
-    after do
-      User.current = nil
     end
     context 'og_type tag' do
       let(:clip) {Clip.new(:url => 'http://example.com/', :og_type => 'website')}
       subject{@clip.tags}
       its(:length){should eq 1}
-      it {subject[0].user.should == User.current}
+      it {subject[0].user.should == @user}
       it {subject[0].name.should == 'website'}
     end
     context 'slide tag' do
       let(:clip) {Clip.new(:url => 'http://example.com/', :og_type => 'slideshare:presentation')}
       subject{@clip.tags}
       its(:length){should eq 2}
-      it {subject[0].user.should == User.current}
+      it {subject[0].user.should == @user}
       it {subject[0].name.should == 'slideshare:presentation'}
-      it {subject[1].user.should == User.current}
+      it {subject[1].user.should == @user}
       it {subject[1].name.should == 'slide'}
     end
   end
   context 'search condition' do
     before do
-      user = create(:user)
-      create_list(:clip,15,user: user)
-      create(:clip, pin: true, user: user)
-      create(:clip, trash: true, user: user)
+      @user = create(:user)
+      create_list(:clip,15,user: @user)
+      create(:clip, pin: true, user: @user)
+      create(:clip, trash: true, user: @user)
     end
     context 'pinned' do
-      subject{Clip.pinned}
+      subject{Clip.pinned @user}
       its(:length){should eq 1}
     end
     context 'trashed' do
-      subject{Clip.trashed}
+      subject{Clip.trashed @user}
       its(:length){should eq 1}
     end
   end
   context 'paging' do
     before do
-      user = create(:user)
-      create_list(:clip,15,user: user)
+      @user = create(:user)
+      create_list(:clip,15,user: @user)
     end
     describe 'first page' do
-      subject{Clip.page}
+      subject{Clip.page @user}
       its(:length){should eq 8}
     end
     describe 'second page' do
-      subject{Clip.page(2)}
+      subject{Clip.page(@user,2)}
       its(:length){should eq 7}
     end
     describe 'third page' do
-      subject{Clip.page(3)}
+      subject{Clip.page(@user,3)}
       its(:length){should eq 0}
     end
   end
   context 'search' do
     before do
-      create(:search_clip)
+      @user = create(:user)
+      clip = create(:search_clip,user: @user)
     end
-    subject {Clip.search 'test'}
+    subject {Clip.search @user,'test'}
     its(:length){should eq 1}
   end
 end
